ec86e6c1ec038f850497e691af040050383cc67d
Brian Dunnette <dunn0172@umn.edu>
2014-09-04 17:04:43 -0500

    updating text of HumanName

---
+++ api/controllers/HumanNameController.js
@@ -0,0 +1,11 @@
+/**
+ * HumanNameController
+ *
+ * @description :: Server-side logic for managing Humannames
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
